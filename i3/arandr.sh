1aeb4f7e21c801b5c380223b2cdbc431b860578d
Daniel Alejandro Gallegos <daniel@tacowolf.net>
2019-05-03 15:19:59 -0400

    restructure i3 display configuration for now

--- i3/arandr.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output HDMI1 --off --output VIRTUAL1 --off --output HDMI-1-2 --mode 1920x1080 --pos 1920x0 --rotate normal
-
-## this is the configuration for my monitor when plugged in
-# this is set using automatically using autorandr.
