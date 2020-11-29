d4d625b2848cc9974107726c92823efcb41ea968
Daniel Alejandro Gallegos <daniel@tacowolf.net>
2020-11-28 19:54:01 -0500

    update gtk settings

---
+++ install.sh
@@ -0,0 +1,13 @@
+
+#!/usr/bin/env bash
+
+# generously adapted from 
+# https://github.com/davatorium/rofi-themes/blob/master/install.sh
+
+
+mkdir -vp ~/.config/kitty
+mkdir -vp ~/.config/sway
+mkdir -vp ~/.config/waybar
+
+## TODO link files using
+# ln -si $(pwd)
